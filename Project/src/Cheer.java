f57e24b00fe91021c6ebf7bcca3933371c202670
ronai22 <ronai22@gmail.com>
2018-07-30 17:03:51 +0200

    a4

--- Project/src/Cheer.java
+++ Project/src/Cheer.java
@@ -1,12 +1,6 @@
 public class Cheer {
 
-<<<<<<< HEAD
-    public static void cheer() {
-        System.out.print("Java");
-=======
     public static void cheer(String message) {
         System.out.print(message);
->>>>>>> a15d2b82c985846d75e30f6687d2717d052a96f2
     }
-
 }
