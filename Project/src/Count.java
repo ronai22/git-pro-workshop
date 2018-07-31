6a3ea30cf96770eca7759f9b6f9fdb84e3d589c4
Matyas Molnar <molnarmatyas92@gmail.com>
2018-07-31 14:12:44 +0200

    b6

--- Project/src/Count.java
+++ Project/src/Count.java
@@ -2,7 +2,7 @@ public class Count {
 
   String param = "param";
   public void count() {
-    System.out.println("9");
+    System.out.println("greeting for the parameter");
     System.out.println(param);
   }
 
